6a341440982304887908431df945e9e9358e6b81
Younes Sennadj <by_sennadj@esi.dz>
2024-10-26 19:31:20 -0700

    Submission for PE03 Younes Sennadj

---
+++ UniversalTranslator/Capabilities/app.py
@@ -0,0 +1,71 @@
+from chalice import Chalice
+from chalicelib import storage_service
+from chalicelib import transcription_service
+from chalicelib import translation_service
+from chalicelib import speech_service
+
+
+import base64
+import json
+
+#####
+# chalice app configuration
+#####
+app = Chalice(app_name='Capabilities')
+app.debug = True
+
+#####
+# services initialization
+#####
+storage_location = 'contents-younes-awsai'
+storage_service = storage_service.StorageService(storage_location)
+transcription_service = transcription_service.TranscriptionService(storage_service)
+translation_service = translation_service.TranslationService()
+speech_service = speech_service.SpeechService(storage_service)
+
+
+#####
+# RESTful endpoints
+#####
+@app.route('/recordings', methods = ['POST'], cors = True)
+def upload_recording():
+    """processes file upload and saves file to storage service"""
+    request_data = json.loads(app.current_request.raw_body)
+    file_name = request_data['filename']
+    file_bytes = base64.b64decode(request_data['filebytes'])
+
+    file_info = storage_service.upload_file(file_bytes, file_name)
+
+    return file_info
+
+
+@app.route('/recordings/{recording_id}/translate-text', methods = ['POST'], cors = True)
+def translate_recording(recording_id):
+    """transcribes the specified audio then translates the transcription text"""
+    request_data = json.loads(app.current_request.raw_body)
+    from_lang = request_data['fromLang']
+    to_lang = request_data['toLang']
+
+    transcription_text = transcription_service.transcribe_audio(recording_id, from_lang)
+
+    translation_text = translation_service.translate_text(transcription_text,
+                                                          target_language = to_lang)
+
+    return {
+        'text': transcription_text,
+        'translation': translation_text
+    }
+
+
+@app.route('/synthesize_speech', methods = ['POST'], cors = True)
+def synthesize_speech():
+    """performs text-to-speech on the specified text / language"""
+    request_data = json.loads(app.current_request.raw_body)
+    text = request_data['text']
+    language = request_data['language']
+
+    translation_audio_url = speech_service.synthesize_speech(text, language)
+
+    return {
+        'audioUrl': translation_audio_url
+    }
---
+++ UniversalTranslator/Website/scripts.js
@@ -0,0 +1,193 @@
+"use strict";
+
+const serverUrl = "127.0.0.1:8080";
+
+class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = "HttpError";
+        this.response = response;
+    }
+}
+
+let audioRecorder;
+let recordedAudio;
+
+const mediaConstraints = {
+    audio: true
+};
+navigator.mediaDevices.getUserMedia(mediaConstraints)
+    .then(onMediaSuccess)
+    .catch(onMediaError);
+
+const maxAudioLength = 30000;
+let audioFile = {};
+
+function onMediaSuccess(audioStream) {
+    audioRecorder = new MediaStreamRecorder(audioStream);
+    audioRecorder.mimeType = "audio/wav";
+    audioRecorder.ondataavailable = handleAudioData;
+}
+
+function onMediaError(error) {
+    alert("audio recording not available: " + error.message);
+}
+
+function startRecording() {
+    recordedAudio = [];
+    audioRecorder.start(maxAudioLength);
+}
+
+function stopRecording() {
+    audioRecorder.stop();
+}
+
+function handleAudioData(audioRecording) {
+    audioRecorder.stop();
+
+    audioFile = new File([audioRecording], "recorded_audio.wav", {type: "audio/wav"});
+
+    let audioElem = document.getElementById("recording-player");
+    audioElem.src = window.URL.createObjectURL(audioRecording);
+}
+
+let isRecording = false;
+
+function toggleRecording() {
+    let toggleBtn = document.getElementById("record-toggle");
+    let translateBtn = document.getElementById("translate");
+
+    if (isRecording) {
+        toggleBtn.value = 'Record';
+        translateBtn.disabled = false;
+        stopRecording();
+    } else {
+        toggleBtn.value = 'Stop';
+        translateBtn.disabled = true;
+        startRecording();
+    }
+
+    isRecording = !isRecording;
+}
+
+async function uploadRecording() {
+    // encode recording file as base64 string for upload
+    let converter = new Promise(function(resolve, reject) {
+        const reader = new FileReader();
+        reader.readAsDataURL(audioFile);
+        reader.onload = () => resolve(reader.result
+            .toString().replace(/^data:(.*,)?/, ''));
+        reader.onerror = (error) => reject(error);
+    });
+    let encodedString = await converter;
+
+    // make server call to upload image
+    // and return the server upload promise
+    return fetch(serverUrl + "/recordings", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({filename: audioFile.name, filebytes: encodedString})
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+let fromLang;
+let toLang;
+
+function translateRecording(audio) {
+    let fromLangElem = document.getElementById("fromLang");
+    fromLang = fromLangElem[fromLangElem.selectedIndex].value;
+    let toLangElem = document.getElementById("toLang");
+    toLang = toLangElem[toLangElem.selectedIndex].value;
+
+    // start translation text spinner
+    let textSpinner = document.getElementById("text-spinner");
+    textSpinner.hidden = false;
+
+    // make server call to transcribe recorded audio
+    return fetch(serverUrl + "/recordings/" + audio["fileId"] + "/translate-text", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({fromLang: fromLang, toLang: toLang})
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function updateTranslation(translation) {
+    // stop translation text spinner
+    let textSpinner = document.getElementById("text-spinner");
+    textSpinner.hidden = true;
+
+    let transcriptionElem = document.getElementById("transcription");
+    transcriptionElem.appendChild(document.createTextNode(translation["text"]));
+
+    let translationElem = document.getElementById("translation");
+    translationElem.appendChild(document.createTextNode(translation["translation"]["translatedText"]));
+
+    return translation
+}
+
+function synthesizeTranslation(translation) {
+    // start translation audio spinner
+    let audioSpinner = document.getElementById("audio-spinner");
+    audioSpinner.hidden = false;
+
+    // make server call to synthesize translation audio
+    return fetch(serverUrl + "/synthesize_speech", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({text: translation["translation"]["translatedText"], language: toLang})
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function updateTranslationAudio(audio) {
+    // stop translation audio spinner
+    let audioSpinner = document.getElementById("audio-spinner");
+    audioSpinner.hidden = true;
+
+    let audioElem = document.getElementById("translation-player");
+    audioElem.src = audio["audioUrl"];
+}
+
+function uploadAndTranslate() {
+    let toggleBtn = document.getElementById("record-toggle");
+    toggleBtn.disabled = true;
+    let translateBtn = document.getElementById("translate");
+    translateBtn.disabled = true;
+
+    uploadRecording()
+        .then(audio => translateRecording(audio))
+        .then(translation => updateTranslation(translation))
+        .then(translation => synthesizeTranslation(translation))
+        .then(audio => updateTranslationAudio(audio))
+        .catch(error => {
+            alert("Error: " + error);
+        })
+
+    toggleBtn.disabled = false;
+}
